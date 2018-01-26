55ffd8e38d68c1ad7c94ed13e1ece88027791c39
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 22:52:57 -0500

    ...

--- A1/sketch.js
+++ A1/sketch.js
@@ -1,37 +1,14 @@
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(640, 480);
 }
 
 function draw() {
-	background(60,50,255);
-
-	fill(0);
-	// textSize(32);
-	// textStyle(Bold);
-	// text('What time is it?', 5, 30);
-
-	textSize(10);
+	background(50,50,255);
 	var h = hour();
-	text('Current hour:\n' + h, 5, 70);
+	text('Hour:\n' + h, 5, 50);
 	var m = minute();
 	text('Minute:\n'+ m, 5, 100);
 	var s = second();
-	text('Second:\n'+ s, 5, 130);
+	text('Second:\n'+ s, 5, 150);
 
-	// var clock_hr = map(h, 0, width, 0, 100);
-	// var clock_min = map(m, 0, width, 0, 100);
-	// var clock_sec = map(s, 0, width, 0, 100);
-
-	// rect(100, 50, clock_hr, 25);
-	// rect(100, 80, clock_min, 25);
-	// rect(100, 120, clock_sec, 25);
-
-	// fill(255,255,0);
-
-	// // translate(width*(2/3), height/2);
-	// ellipse(150, 150, 100, 100);
-
-	// fill(255);
-	// rotate(clock_sec / 3.0);
-	// rect(150, 150, 8, 80);
 }
