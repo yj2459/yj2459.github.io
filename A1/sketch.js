45a1b8fa0d11dcc69bbeb64b609732d201c9834b
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 23:07:39 -0500

    workplease

--- A1/sketch.js
+++ A1/sketch.js
@@ -4,12 +4,38 @@ function setup() {
 }
 
 function draw() {
-	
+	textSize(32);
+	textStyle(BOLD);
+	text('What Time Is It?', 5, 25);
+
+	textSize(10);
 	var h = hour();
 	text('Hour:\n' + h, 5, 50);
 	var m = minute();
 	text('Minute:\n'+ m, 5, 100);
 	var s = second();
 	text('Second:\n'+ s, 5, 150);
+
+	var clock_hr = map(h, 0, width, 0, 100);
+	var clock_min = map(m, 0, width, 0, 100);
+	var clock_sec = map(s, 0, width, 0, 100);
+
+	rect(100, 50, clock_hr, 25);
+	rect(100, 80, clock_min, 25);
+	rect(100, 120, clock_sec, 25);
+
+	push();
+	fill(255, 255, 0);
+	translate(width*(2/3), height/2);
+	ellipse(150, 150, 500, 500);
+	pop();
+
+	push();
+	fill(255);
+	rotate(clock_sec, 3.0);
+	rect(150, 150, 8, 80);
+	pop();
+	
+
 
 }
