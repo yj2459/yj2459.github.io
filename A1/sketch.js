a1c7dfc7436089f7075f7211eccae21f32f27783
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 20:43:34 -0500

    try1

--- A1/sketch.js
+++ A1/sketch.js
@@ -7,7 +7,7 @@ function draw() {
 	var h = hour();
 	text('Current hour:\n' + h, 5, 50);
 	var m = minute();
-	text('Current minute:\n'+ m, 5, 50);
+	text('Current minute:\n'+ m, 5, 100);
 	var s = second();
 	text('Current second:\n'+ s, 5, 50);
 
