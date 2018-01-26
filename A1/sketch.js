4ac8e8da772eba0bf481fe3ce43268c1057a4fc5
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 22:58:05 -0500

    r-4

--- A1/sketch.js
+++ A1/sketch.js
@@ -1,15 +1,16 @@
 function setup() {
   createCanvas(640, 480);
+  background(50,50,255);
 }
 
 function draw() {
-	background(50,50,255);
+
 	textSize(32)
 	textStyle(Bold);
 	text('WHAT TIME IS IT?', 5, 30)
 
 	var h = hour();
-	text('Hour:\n' + h, 5, 50);
+	text('Current hour:\n' + h, 5, 50);
 	var m = minute();
 	text('Minute:\n'+ m, 5, 100);
 	var s = second();
