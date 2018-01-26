0cf55a653e19ef8663f1d5455488e8071fdd6258
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 22:01:12 -0500

    trial4

--- A1/sketch.js
+++ A1/sketch.js
@@ -4,17 +4,16 @@ function setup() {
 
 function draw() {
 	background(50,50,255);
-	fill(0)
-	textSize(32)
-	text('What time is it?', 5, 10)
+	fill(0);
+	textSize(32);
 	var h = hour();
 	text('Current hour:\n' + h, 5, 30);
 	var m = minute();
 	text('Current minute:\n'+ m, 5, 70);
 	var s = second();
 	text('Current second:\n'+ s, 5, 110);
-	var clock_sec = map(s, 0, width, 100, 500)
-	var clock_min = map(m, 0, 60, 0, 60)
+	var clock_sec = map(s, 0, width, 100, 500);
+	var clock_min = map(m, 0, 60, 0, 60);
 
 	rect(25, 100, 25, clock_sec * 5)
 	fill(255,255,0)
