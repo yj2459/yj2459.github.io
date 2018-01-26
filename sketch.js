2f0b63ffd7808a0931a997a4845dc9606c128960
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-25 20:21:08 -0500

    hour

--- sketch.js
+++ sketch.js
@@ -3,28 +3,6 @@ function setup() {
 }
 
 function draw() {
-	background('purple');
-	//see http://p5js.org/reference/#/p5/background
-	fill(255,250,67);
-	rect(180,180,10,10);
-	rect(200,180,10,10);
-	rect(220,180,10,10);
-	rect(240,180,10,10);
-	fill(255,90,153)
-	ellipse(30,10,50,50);
-	ellipse(30,60,50,50);
-	ellipse(30,110,50,50);
-	ellipse(30,160,50,50);
-	ellipse(30,210,50,50);
-	ellipse(30,260,50,50);
-	ellipse(30,310,50,50);
-	ellipse(30,360,50,50);
-	ellipse(30,410,50,50);
-	ellipse(30,460,50,50);
-	textSize(50);
-	fill(0);
-	text('Yuhwa is here',184,254);
-	fill(255);
-	text('Yuhwa is here',180,250);
-	
+	var h = hour();
+	text('Current hour:\n' + h, 5, 50);
 }
