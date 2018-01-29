7011dae5163f0eedd6654267358ce204c819ddcf
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-29 17:38:26 -0500

    guide_lines_01

--- clock02/sketch.js
+++ clock02/sketch.js
@@ -10,9 +10,19 @@ function draw() {
 
 	var color_opacity = map(s, 0, 60, 0, 60)
 	
-	fill(0, 0, 0, 50)
+	fill(0, 0, 0, 20)
 	rect(20, 20, 100, 400)
 	rect(140, 20, 500, 400)
 	rect(660, 20, 200, 400)
+
+	#these are lines for reference
+	var Sline = line(50, 50, 90, 50, 50)
+	var Dline = line(50, 390, 90, 390, 50)
+
+	arrayCopy(Sline, Dline, [60])
+
+
+
+
 
 }
