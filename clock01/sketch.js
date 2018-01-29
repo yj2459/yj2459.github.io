c465e8aa009211a12693acb99b7f03902f5b9dea
Yuhwa Jeong <yj2459@columbia.edu>
2018-01-29 14:55:27 -0500

    02

--- clock01/sketch.js
+++ clock01/sketch.js
@@ -8,7 +8,7 @@ function draw() {
 	var m = minute();
 	var s = second();
 
-	var color_opacity = map(s, 0, 100, 0, width)
+	var color_opacity = map(s, 0, 60, 0, 60)
 	fill(0,0,0,color_opacity)
 	triangle(30,75,58,20,86,75)
 
