f4a7125cc966fdca5a73ef536ff3d124b7a1ca97
Yuhwa Jeong <yj2459@columbia.edu>
2018-02-03 17:55:41 -0500

    Yuhwa's homepage

--- clock01/sketch.js
+++ clock01/sketch.js
@@ -10,40 +10,43 @@ function draw() {
 
 	// this is clock 01 : triangle clock
 
+	//time text
 	fill(0)
 	strokeWeight(0);
 	textSize(12);
-	text(h, 30, 30);
-	text(':', 50, 30);
-	text(m, 80, 30);
-	text(':', 110, 30);
-	text(s, 140, 30);
+	text(h + ':' + m + ':' + s, 30, 30)
 
+	//second
 	var color_opacity = map(s, 0, 60, 255, 0);
-	
 	fill(color_opacity);
 	strokeWeight(0)
 	triangle(70,115,98,60,126,115);
 
-
-	if (0<m<=20){
-		strokeWeight(3);
+	//minute
+	if (0<m<21){
+		strokeWeight(5);
 		line(98, 60, 98 + (28/20)*m, 60 + (55/20)*m);
-	} else if (20<<m<=40){
-		strokeWeight(3);
+	} else if (20<m<41){
+		strokeWeight(5);
 		line(98,60,126,115);
 		line(126, 115, 126 - (56/20)*m, 115);
 	} else {
-		strokeWeight(3);
+		strokeWeight(5);
 		line(98,60,126,115);
 		line(126,115,70,115);
 		line(70, 115, 70 + (28/20)*(m-40), 115 - (55/20)*(m-40));
 	}
 
+	//hour
+	//hourpoint
 	fill(255, 20, 250);
 	noStroke()
 	ellipse(98, 60, 7, 7);
 
+
+	// //hour rotation
+	// var hour_rotation = m(h, 0, 24, 0, 2*PI)
+	// rotate(hour_rotation)
 
 
 }
