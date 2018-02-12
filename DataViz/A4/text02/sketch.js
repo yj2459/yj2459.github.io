bb807f10fb33693766e9e1d535de9e09f6e3ea11
Yuhwa Jeong <yj2459@columbia.edu>
2018-02-12 16:59:56 -0500

    text02

--- DataViz/A4/text02/sketch.js
+++ DataViz/A4/text02/sketch.js
@@ -1,25 +1,13 @@
-//Question :: How to load font ? .ttf
-
-//count text frequency
-//make an array with them
-//when mousePressed, it will be colored in order
-
-
-var myFont;
 var headlines = [];
-var section = [];
-//var subsection = [];
-var maxHeadLen, minHeadLen;
 var hitwords = [
-  "World",
-  "Olympic"
+  "world",
+  "olympics"
 ];
 
 function preload() {
-  //myFont = loadFont('LeagueMono-Light.ttf');
 
   // Assemble url for API call
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+  var url = "https://api.nytimes.com/svc/topstories/v2/opinion.json";
   var apikey = "e0b773a6804541fcb64d23e7c14bc2fd"; // see: https://developer.nytimes.com
   url += "?api-key=" + apikey;
 
@@ -29,11 +17,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1000, 900);
+  createCanvas(640, 850);
   background(0);
 
   textSize(10);
-  //textFont(myFont);
   textAlign(LEFT);
 
   noLoop(); // since we're not animating, one frame is sufficient: run draw() just once
@@ -44,97 +31,38 @@ function setup() {
 function draw() {
   background(0);
 
-  // Set the left and top margin
+  var lineheight = 20;
   var margin = 40;
   translate(margin, margin);
 
-  var lineheight = 19;
-  var rectheight = 8;
-
   for (var i = 0; i < headlines.length; i++) {
-    var wordsH = split(headlines[i], ' ');
-    var wordsS = split(section[i], ' ');
+    var words = split(headlines[i], ' ');
+
     var nextX = 0;
-    console.log(wordsS);
 
-    // draw rectangle
-    // fill(120);
-    // var rectwidth = map(headlines[i].length, minHeadLen, maxHeadLen, margin, width-margin-200);
-    // rect(0, i*lineheight, rectwidth, -1*rectheight)
-
-    // draw headline, section, subsection
-    // fill(255);
-    // text(headlines[i], 0, i*lineheight);
-    // text(section[i], 450, i*lineheight);
-    // text(subsection[i], 500, i*lineheight);
-
-    // if (hitwords.includes(section[i])) {
-    //   fill("orange");
-    // } else {
-    //   fill(255);
-    // }
-
-    for (var j = 0; j < wordsH.length; j++) {
-      if (hitwords.includes(wordsH[j],wordsS[i])) {
-        fill(200, 100, 200);
+    for (var j = 0; j < words.length; j++) {
+      if (hitwords.includes(words[j].toLowerCase())) {
+        fill("orange");
       } else {
-        fill(255)
+        fill(255);
       }
 
-      text(wordsH[j]+' ', nextX, i*lineheight);
-      //text(wordS[j], 500, i*lineheight);
-      nextX += textWidth(wordsH[j]+' ');
+      text(words[j]+' ', nextX, i*lineheight);
+      nextX += textWidth(words[j]+' ');
     }
-
-    text(wordsS[i], 500, 0);
-    // for (var j = 0; j < section.length; j++) {
-    //   if (hitwords.includes(section[j])) {
-    //     fill(200, 100, 200);
-    //   } else {
-    //     fill(255)
-    //   }
-
-    //   text(section[j]+' ', nextX, i*lineheight);
-    //   nextX += textWidth(section[j]+' ');
-    // }
   }
-
-  text(section, 500, 0);
-
-  
 }
 
 function extractHeadlines() {
 
-  //console.log(nytResponse); // take a look at the full API response structure
+  // console.log(nytResponse); // take a look at the full API response structure
 
-  for (var i = 0; i < (nytResponse.results.length); i++) {
-    var sec = nytResponse.results[i].section;
-    //var ss = nytResponse.results[i].subsection;
+  for (var i = 0; i < nytResponse.results.length; i++) {
     var h = nytResponse.results[i].title;
-  
     // besides .title, other text data available to you include:
     // .abstract, .byline, .section, etc. etc.
-
-    if (!maxHeadLen) {
-      maxHeadLen = h.length;
-    } else if (h.length > maxHeadLen) {
-      maxHeadLen = h.length;
-    }
-
-    if (!minHeadLen) {
-      minHeadLen = h.length;
-    } else if (h.length < minHeadLen) {
-      minHeadLen = h.length;
-    }
-
     append(headlines, h);
-    //append(headlines, ss);
-    append(section, sec);
-    //console.log(sec);
   }
 
-  //console.log(headlines); // make sure counted data looks as expected
-  //console.log(maxHeadLen);
-  //console.log(minHeadLen);
+  // console.log(headlines); // make sure counted data looks as expected
 }
